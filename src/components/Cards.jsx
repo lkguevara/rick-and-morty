316bbe98319dd03116017f739d7b10d55041d743
Lian Katherin Guevara <liann.guevara@gmail.com>
2023-02-01 09:43:17 -0500

    responsive cards sm

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -4,7 +4,7 @@ import { IconButton } from "@material-tailwind/react";
 export default function Cards({characters, onClose}) {
    
    return (
-      <div className=" grid grid-cols-3 sm:grid-cols-2 lg:grid-cols-3 gap-12 p-6 justify-items-center">
+      <div className=" grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-12 p-6 justify-items-center">
          {
             characters.map((character) => (
             <Card 
